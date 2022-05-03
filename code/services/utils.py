ef6ae5af48dc6aae920202c823f51a104d5419fe
pablojjimenez <pablojjimenez0@gmail.com>
2022-05-03 21:09:43 +0200

    TFG-prediction fix tests

--- code/services/utils.py
+++ code/services/utils.py
@@ -6,8 +6,6 @@ def assure_exists_directory(directory: str):
     Create directory if not exists
     :param directory: directory name
     """
-    script_dir = os.path.dirname(__file__)
-    results_dir = os.path.join(script_dir, directory)
-
-    if not os.path.isdir(results_dir):
-        os.makedirs(results_dir)
+    isExist = os.path.exists(directory)
+    if not isExist:
+        os.makedirs(directory, exist_ok=False)
