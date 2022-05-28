07b3980aa9d9cd32fae0224f63ac219143f6a628
pablojjimenez <pablojjimenez0@gmail.com>
2022-05-28 16:03:39 +0200

    TFG-REST refactor tests path

---
+++ code/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from tests.mocks.mock_ccaa_repository import MockCcaaRepository
+from tests.mocks.mock_cie_repository import MockCieRepository
+from tests.mocks.mock_disease_repository import MockDiseaseRepository
+from tests.mocks.mock_gedad_repository import MockGedadRepository
+from tests.mocks.mock_raziel_repository import MockRazielRepository
+
+
+@pytest.fixture
+def mock_raziel_repo():
+    return MockRazielRepository(
+        disease_repo=MockDiseaseRepository(MockCieRepository()),
+        ccaas_repo=MockCcaaRepository(),
+        gedades_repo=MockGedadRepository()
+    )
+
+
+@pytest.fixture
+def mock_disease():
+    return MockDiseaseRepository(MockCieRepository())
