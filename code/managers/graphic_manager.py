7ac68c2a729e5f032d7915d5d575df7ccff3e8a5
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-04 20:07:44 +0200

    TFG-REST little refactor

--- code/managers/graphic_manager.py
+++ code/managers/graphic_manager.py
@@ -1,22 +1,24 @@
 from matplotlib import pyplot
-from services.utils import assure_exists_directory
 
+from managers.utils import assure_exists_directory
 from repositories.abstract_repository import ListParams
 from repositories.raziel_repository import RazielRepository
 
 
 class GraphicManager:
+    CHART_PATH = 'opt/chart.png'
+
     def __init__(self, raziel_repo: RazielRepository):
         self.raziel_repo = raziel_repo
 
     def get_chart_by_two_vars(self, params: ListParams, var1: str, var2: str):
-        df = self.raziel_repo.prepare_and_gruping_dataframe(params, var1, var2)
+        df = self.raziel_repo.prepare_and_grouping_dataframe(params, var1, var2, True)
         df.plot()
         assure_exists_directory('opt/')
-        pyplot.savefig('opt/get_chart_by_two_vars.png')
+        pyplot.savefig(GraphicManager.CHART_PATH)
 
     def get_bar_chart_by_two_vars(self, params: ListParams, var1: str, var2: str):
-        df = self.raziel_repo.prepare_and_gruping_dataframe(params, var1, var2)
-        df.plot.pie()
+        df = self.raziel_repo.prepare_and_grouping_dataframe(params, var1, var2)
+        df.plot.bar()
         assure_exists_directory('opt/')
-        pyplot.savefig('opt/get_bar_chart_by_two_vars.png')
+        pyplot.savefig(GraphicManager.CHART_PATH)
--- code/managers/predictor_manager.py
+++ code/managers/predictor_manager.py
@@ -8,12 +8,15 @@ from repositories.raziel_repository import RazielRepository
 
 
 class PredictorManager:
+    CHART_PATH = 'opt/forecasting.png'
+
     def __init__(self, raziel_repo: RazielRepository):
         self.raziel_repo = raziel_repo
 
-    def deaths_forecasting(self, params: ListParams, var1: str, var2: str):
+    def deaths_forecasting(self, params: ListParams, var1: str, var2: str, period: int):
         """
         Compute the next values of a dataframe
+        :param period: forecasting future
         :param params: ListParams
         :param var1: group by value
         :param var2: projection value
@@ -26,7 +29,7 @@ class PredictorManager:
         df2 = df.copy()
         df2['ds'] = pd.to_datetime(df2['year'], format='%Y')
         m = Prophet().fit(df2)
-        future = m.make_future_dataframe(periods=50, freq='y', include_history=True)
+        future = m.make_future_dataframe(periods=period, freq='y', include_history=True)
         fcst = m.predict(future)
 
         m.plot(fcst)
