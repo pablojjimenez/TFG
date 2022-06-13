efc8c8aebe5288ebf1f96c60f27d29a16c257ff9
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-13 07:39:13 +0200

    TFG-GRAPHQL Add graphql query ide to api routes

--- code/app.py
+++ code/app.py
@@ -1,9 +1,13 @@
+import strawberry
 from fastapi import FastAPI
 
 from starlette.requests import Request
 from fastapi.exceptions import HTTPException
 from starlette.responses import Response, JSONResponse
+from strawberry.asgi import GraphQL
 
+from graphql_services.graph_types import Query
+from graphql_services.graphql_mutation import Mutation
 from services.disease_service import dataRouter
 from services.managers_service import managersRouter
 
@@ -17,8 +21,15 @@ class validation_exception_handler:
             }, status_code=400)
 
 
+# Create FastAPI app
 app = FastAPI()
 app.include_router(dataRouter, tags=["Api resources"], prefix="/data")
 app.include_router(managersRouter, tags=["Api managers"], prefix="/managers")
-
 app.add_exception_handler(HTTPException, validation_exception_handler())
+
+# Add GraphQL to server app
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+graphql_app = GraphQL(schema)
+
+app.add_route("/graphql", graphql_app)
+#app.add_websocket_route("/graphql_services", graphql_app)
