7d47629827f536817a74efaade46abc95b1716b4
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-13 07:37:02 +0200

    TFG-GRAPHQL Add utils functions to work with pydicts

--- code/managers/utils.py
+++ code/managers/utils.py
@@ -17,6 +17,36 @@ def transform_params(query: Dict[str, Dict[str, str]],
     return {
         'query': query,
         'sort': sort,
-        'limit': limit,
+        'limit': limit if limit <= 100 else 100,
         'page': page
     }
+
+
+def remove_nulls_from_json(data: dict):
+    dd = {}
+    for k, v in data.items():
+        if isinstance(v, dict):
+            r = remove_nulls_from_json(v)
+            if len(r) != 0:
+                dd[k] = r
+        elif v is not None or k == 'neq':
+            dd[k] = v
+    return dd
+
+
+def change_key_operators(data: dict):
+    changes = {
+        'eq': '==',
+        'gt': '>',
+        'lt': '<',
+        'neq': '!='
+    }
+    items = list(data.items())
+    for k, v in items:
+        if isinstance(v, dict):
+            change_key_operators(v)
+        else:
+            if k in list(map(lambda x: x[0], items)):
+                data[changes[k]] = data[k]
+                del data[k]
+    return data
