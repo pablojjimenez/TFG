5666a322b470b91e41347e47924ed931f60afac7
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-14 18:55:25 +0200

    TFG-73 Add operation to delete files older than 5 min

--- code/managers/utils.py
+++ code/managers/utils.py
@@ -1,5 +1,8 @@
 import os
 from typing import Dict
+import arrow
+
+from config.general_config import CHARTS_BASE_PATH
 
 
 def ensure_directory_exists(directory: str):
@@ -10,6 +13,17 @@ def ensure_directory_exists(directory: str):
     is_exist = os.path.exists(directory)
     if not is_exist:
         os.makedirs(directory, exist_ok=False)
+    remove_pasts_images()
+
+
+def remove_pasts_images():
+    deleteTime = arrow.now().shift(minutes=-5)
+    for f in os.listdir(CHARTS_BASE_PATH):
+        f = os.path.join(CHARTS_BASE_PATH, f)
+        itemTime = arrow.get(os.stat(f).st_mtime)
+        if itemTime < deleteTime:
+            if os.path.isfile(f):
+                os.remove(f)
 
 
 def transform_params(query: Dict[str, Dict[str, str]],
