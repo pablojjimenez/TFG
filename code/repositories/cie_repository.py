c86aeb9618c395c2c509b56555bff6d26446c4d4
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-26 19:43:10 +0200

    TFG-graphql add search logic to DiseaseRepository

--- code/repositories/cie_repository.py
+++ code/repositories/cie_repository.py
@@ -6,27 +6,11 @@ class CieRepository(AbstractRepository):
     def __init__(self, name):
         super().__init__(name)
 
-    @staticmethod
-    def get_query_from_cie(cie):
-        out = {}
-        if cie.id is not None:
-            out.update({'id': {'like': cie.id}})
-        if cie.description is not None:
-            out.update({'description': {'like': cie.description}})
-        return {
-            'query': out
-        }
-
     def get_all(self, params: ListParams = None) -> (list, int):
         data = super().get_all(params)
-        objs = list(map(lambda x: Cie(id=x[0], description=x[1]), data))
+        objs = list(map(lambda x: Cie(id=x[0], name=x[1]), data))
         return objs, len(data)
-
-    def get_like(self, cie: Cie):
-        data = super().get_all(CieRepository.get_query_from_cie(cie))
-        objs = list(map(lambda x: Cie(id=x[0], description=x[1]), data))
-        return objs
 
     def get_one(self, id: str) -> object:
         data = super().get_one(id)
-        return Cie(id=data[0], description=data[1]) if data else None
+        return Cie(id=data[0], name=data[1]) if data else None
--- code/repositories/disease_repository.py
+++ code/repositories/disease_repository.py
@@ -10,23 +10,13 @@ class DiseaseRepository(AbstractRepository):
         super().__init__(name)
         self.cie_repo = cie_repo
 
-    def get_all(self, params: ListParams = None, cie: Cie = None) -> Tuple[List[Disease], int]:
-        cies = None
-        if cie is not None:
-            cies = self.cie_repo.get_like(cie)
-
-        if cie is not None and len(cies) > 0:
-            cie_queries = list(map(lambda x: x.id, cies))
-            params['query'] = {
-                'cie': {
-                    '==': cie_queries
-                },
-            }
+    def get_all(self, params: ListParams = None,
+                cies: Tuple[List[Cie], int] = None) -> Tuple[List[Disease], int]:
+        if cies is not None and cies[1] > 0:
+            cie_queries = list(map(lambda x: x.id, cies[0]))
+            params['query']['cie'] = {'==': cie_queries}
         data = super().get_all(params)
-        print(cies)
-
         data_objs = list(map(lambda x: Disease(x[0], x[1], self.cie_repo.get_one(x[2])), data))
-        print(data_objs)
         return data_objs, len(data)
 
     def get_one(self, id: int) -> object:
