3d6d7aa2832bf44241bb5bb156311135cc0054fe
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-26 12:21:46 +0200

    TFG-graphql add graphql types

---
+++ code/graphql_services/grapql_types.py
@@ -0,0 +1,146 @@
+import strawberry
+import typing
+
+from dataclasses import asdict
+
+from managers.utils import transform_params
+from models.small_models import Cie
+from repositories.creator import DiseaseRepoCreator, AgesGroupsRepoCreator, CieRepoCreator, CcaaRepoCreator
+
+T = typing.TypeVar("T")
+
+
+@strawberry.type
+class CcaaDTO:
+    id: str
+    name: str
+
+
+@strawberry.type
+class CieDTO:
+    id: str
+    description: str
+
+
+@strawberry.type
+class DiseaseDTO:
+    id: str
+    name: str
+    cie: typing.Optional[CieDTO]
+
+
+@strawberry.type
+class GedadDTO:
+    id: str
+    description: str
+
+
+@strawberry.type
+class RazielDTO:
+    ano: int
+    causa: DiseaseDTO
+    sx: int
+    ccaa: CcaaDTO
+    gedad: GedadDTO
+    defu: int
+    avp: int
+    cruda: float
+    tavp: float
+    edad: float
+    tasae: float
+    tavpe: float
+    tasaw: float
+    tasavpw: float
+    id: int
+
+
+@strawberry.type
+class MyReturnType(typing.Generic[T]):
+    items: typing.List[T]
+    length: int
+
+
+p = transform_params(None, None, None, None)
+
+
+def get_ccaas():
+    rtado = CcaaRepoCreator().get_all_operation(p)
+    return MyReturnType[CcaaDTO](rtado[0], rtado[1])
+
+
+def get_cies():
+    rtado = CieRepoCreator().get_all_operation(p)
+    return MyReturnType[CieDTO](rtado[0], rtado[1])
+
+
+def get_diseases():
+    rtado = DiseaseRepoCreator().get_all_operation(p)
+    return MyReturnType[DiseaseDTO](rtado[0], rtado[1])
+
+
+def get_ages_groups():
+    rtado = AgesGroupsRepoCreator().get_all_operation(p)
+    return MyReturnType[GedadDTO](rtado[0], rtado[1])
+
+
+@strawberry.type
+class Query:
+    ccaas: MyReturnType[CcaaDTO] = strawberry.field(resolver=get_ccaas)
+    cies: MyReturnType[CieDTO] = strawberry.field(resolver=get_cies)
+    diseases: MyReturnType[DiseaseDTO] = strawberry.field(resolver=get_diseases)
+    ages_groups: MyReturnType[GedadDTO] = strawberry.field(resolver=get_ages_groups)
+
+
+@strawberry.type
+class MyQuery:
+    query: typing.Dict
+
+
+@strawberry.input
+class FilterOperators(typing.Generic[T]):
+    eq: typing.Optional[T] = None
+    gt: typing.Optional[T] = None
+    lt: typing.Optional[T] = None
+    neq: typing.Optional[T] = None
+    like: typing.Optional[T] = None
+
+
+class Filter:
+    def dict_repr(self) -> dict:
+        return {k: v for k, v in asdict(self).items() if self.__dataclass_fields__[k].repr}
+
+
+@strawberry.input
+class CCAAFilter(Filter):
+    id: typing.Optional[FilterOperators[int]] = None
+    name: typing.Optional[FilterOperators[str]] = None
+
+
+@strawberry.input
+class DiseasesFilter(Filter):
+    id: typing.Optional[FilterOperators[int]] = None
+    name: typing.Optional[FilterOperators[str]] = None
+    cie_id: typing.Optional[str] = None
+    cie_name: typing.Optional[str] = None
+
+    def to_cie(self):
+        return Cie(id=self.cie_id, description=self.cie_name) if self.cie_name or self.cie_id else None
+
+
+@strawberry.input
+class RazielFilter(Filter):
+    id: typing.Optional[FilterOperators[int]] = None
+    ano: typing.Optional[FilterOperators[int]] = None
+    causa: typing.Optional[DiseasesFilter] = None
+    sx: typing.Optional[FilterOperators[float]] = None
+    ccaa: typing.Optional[FilterOperators[int]] = None
+    gedad: typing.Optional[FilterOperators[int]] = None
+    defu: typing.Optional[FilterOperators[int]] = None
+    avp: typing.Optional[FilterOperators[int]] = None
+    cruda: typing.Optional[FilterOperators[float]] = None
+    tavp: typing.Optional[FilterOperators[float]] = None
+    edad: typing.Optional[FilterOperators[float]] = None
+    tasae: typing.Optional[FilterOperators[float]] = None
+    tavpe: typing.Optional[FilterOperators[float]] = None
+    tasaw: typing.Optional[FilterOperators[float]] = None
+    tasavpw: typing.Optional[FilterOperators[float]] = None
