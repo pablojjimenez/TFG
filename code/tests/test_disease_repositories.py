1b8187c8908b84ac80591808e53ff9acb882779c
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-27 10:22:25 +0200

    TFG-97 refactor tests

--- code/tests/test_disease_repositories.py
+++ code/tests/test_disease_repositories.py
@@ -1,8 +1,16 @@
+import pytest
+
 from models.disease_model import Disease
+from models.exceptions import DataIsNotAvailable
 from models.small_models import Cie
+from repositories.disease_repository import DiseaseRepository
 
 
 class TestDiseaseRepo:
+    def test_create_reopsitory_from_blank_directory(self):
+        with pytest.raises(DataIsNotAvailable):
+            DiseaseRepository('directory/fake', None)
+
     def test_get_one_uncomplete_disease_object(self, mock_disease):
         obj = mock_disease.get_one(2)
         assert isinstance(obj, Disease)
