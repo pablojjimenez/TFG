6c99fdb40c80598fe2f32c034a7bcf56fb82f506
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-27 10:24:22 +0200

    TFG-97 add mechanism to load test data according to environment

--- code/config/general_config.py
+++ code/config/general_config.py
@@ -8,11 +8,17 @@ class Settings(BaseSettings):
     port: int = 0
     debug_app: str = ''
     chart_path_base: str = '/tmp'
+    environment: str = 'T'
     ccaa_repo_path: str = ''
     cie_repo_path: str = ''
     gedad_repo_path: str = ''
     disease_repo_path: str = ''
     raziel_repo_path: str = ''
+    mock_ccaa_repo_path: str = 'tests/data/ccaas'
+    mock_cie_repo_path: str = 'tests/data/cie'
+    mock_gedad_repo_path: str = 'tests/data/grupos_edad'
+    mock_disease_repo_path: str = 'tests/data/diseases'
+    mock_raziel_repo_path: str = 'tests/data/razielM'
 
     class Config:
         env_file = '.env'
