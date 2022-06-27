b68b4e07e263ee4bf353cfa085284bceb2375be7
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-27 10:23:23 +0200

    TFG-97 remove useless code

--- code/managers/utils.py
+++ code/managers/utils.py
@@ -12,21 +12,3 @@ def transform_params(query: Dict[str, Dict[str, str]],
         'page': page
     }
 
-
-def call_repository_get_all(repo: AbstractRepository, query: Dict[str, Dict[str, str]], sort: str,
-                            page: int, limit: int):
-    """
-    Aux function used by all endpoints to generate queries
-    :param repo: concerned repository
-    :param query: query dict
-    :param sort: sort param
-    :param page: number page param
-    :param limit: recovery limit number
-    :return: {items, length} return format
-    """
-    p = transform_params(query, sort, page, limit)
-    objs, tam = repo.get_all(p)
-    return {
-        'items': objs,
-        'length': tam
-    }
