198260c47b7c5ee1a35e3259680222109fed4155
pablojjimenez <pablojjimenez0@gmail.com>
2022-06-28 21:18:23 +0200

    TFG-92 add default config

--- code/config/general_config.py
+++ code/config/general_config.py
@@ -4,16 +4,16 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    host: str = ''
+    host: str = '0.0.0.0'
     port: int = 5000
     openapi_path: str = '/docs'
-    debug_app: str = ''
+    debug_app: str = 'F'
     chart_path_base: str = '/tmp'
-    ccaa_repo_path: str = ''
-    cie_repo_path: str = ''
-    gedad_repo_path: str = ''
-    disease_repo_path: str = ''
-    raziel_repo_path: str = ''
+    ccaa_repo_path: str = 'data/ccaas'
+    cie_repo_path: str = 'data/cie'
+    gedad_repo_path: str = 'data/grupos_edad'
+    disease_repo_path: str = 'data/diseases'
+    raziel_repo_path: str = 'data/raziel'
 
     class Config:
         env_file = '.env'
